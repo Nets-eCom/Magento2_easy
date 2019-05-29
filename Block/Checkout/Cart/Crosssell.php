6ff762300c27bf502cf8088375e05ffc45ed13d4
KasemNaoui <belgacem@nordicwebteam.se>
2019-05-29 14:56:43 +0000

    crosssell slider design

--- Block/Checkout/Cart/Crosssell.php
+++ Block/Checkout/Cart/Crosssell.php
@@ -3,5 +3,5 @@ namespace Dibs\EasyCheckout\Block\Checkout\Cart;
 
 class Crosssell extends \Magento\Checkout\Block\Cart\Crosssell
 {
-    protected $_maxItemCount = 8; // Todo: Added to system.xml
+    protected $_maxItemCount = 8; // Todo: Add this to system.xml
 }
