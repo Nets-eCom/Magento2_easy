22b05586dd065f0268a4b738aeedd1c2425e8536
KasemNaoui <belgacem@nordicwebteam.se>
2019-06-03 15:07:56 +0000

    mobile cart / checkout extra part

--- Block/Checkout/Cart/Crosssell.php
+++ Block/Checkout/Cart/Crosssell.php
@@ -1,7 +1,67 @@
 <?php
+
 namespace Dibs\EasyCheckout\Block\Checkout\Cart;
+
+use Dibs\EasyCheckout\Helper\Cart as DibsCartHelper;
+use Magento\CatalogInventory\Helper\Stock as StockHelper;
 
 class Crosssell extends \Magento\Checkout\Block\Cart\Crosssell
 {
-    protected $_maxItemCount = 8; // Todo: Add this to system.xml
+    protected $_maxItemCount;
+
+    /**
+     * @var DibsCartHelper
+     */
+    protected $dibsCartHelper;
+
+    /**
+     * Crosssell constructor.
+     * @param \Magento\Catalog\Block\Product\Context $context
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Magento\Catalog\Model\Product\Visibility $productVisibility
+     * @param \Magento\Catalog\Model\Product\LinkFactory $productLinkFactory
+     * @param \Magento\Quote\Model\Quote\Item\RelatedProducts $itemRelationsList
+     * @param StockHelper $stockHelper
+     * @param DibsCartHelper $_cartHelper
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Catalog\Block\Product\Context $context,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Catalog\Model\Product\Visibility $productVisibility,
+        \Magento\Catalog\Model\Product\LinkFactory $productLinkFactory,
+        \Magento\Quote\Model\Quote\Item\RelatedProducts $itemRelationsList,
+        StockHelper $stockHelper,
+        DibsCartHelper $dibsCartHelper,
+        array $data = []
+    )
+    {
+        parent::__construct(
+            $context,
+            $checkoutSession,
+            $productVisibility,
+            $productLinkFactory,
+            $itemRelationsList,
+            $stockHelper,
+            $data
+        );
+        $this->dibsCartHelper = $dibsCartHelper;
+        $this->_maxItemCount = $dibsCartHelper->getNumberOfCrosssellProducts();
+    }
+
+    /**
+     * @return DibsCartHelper
+     */
+    public function getDibsCartHelper()
+    {
+        return $this->dibsCartHelper;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function isEnable()
+    {
+        return $this->getDibsCartHelper()->isDisplayCrosssell();
+    }
 }
--- Helper/Cart.php
+++ Helper/Cart.php
@@ -14,6 +14,10 @@ class Cart extends AbstractHelper
 {
 
     /**
+     * dibs_easycheckout/crosssell/
+     */
+    const XML_PATH_CROSSSELL = 'dibs_easycheckout/crosssell/';
+    /**
      * @var StoreManagerInterface
      */
     protected $storeManager;
@@ -32,8 +36,38 @@ class Cart extends AbstractHelper
         parent::__construct($context);
     }
 
+    /**
+     * @return Current Currency code
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     public function getCurrencyCode()
     {
         return $this->storeManager->getStore()->getCurrentCurrency()->getCode();
+    }
+
+    /**
+     * @param null $store
+     * @return mixed
+     */
+    public function isDisplayCrosssell($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_CROSSSELL . 'display_crosssell',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $store
+        );
+    }
+
+    /**
+     * @param null $store
+     * @return mixed
+     */
+    public function getNumberOfCrosssellProducts($store = null)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_CROSSSELL . 'crosssell_limit',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $store
+        );
     }
 }
