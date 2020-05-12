bc1443ae8850403768d9a151cd6ec7d2c162914e
maxwhite <maxwhite1983@gmail.com>
2020-05-12 12:02:40 +0300

    added USD currency

--- Model/Dibs/Locale.php
+++ Model/Dibs/Locale.php
@@ -23,7 +23,7 @@ class Locale
      * @var array $allowedCurrencies
      */
     protected $allowedCurrencies = [
-      "SEK","NOK","DKK","EUR"
+      "SEK","NOK","DKK","EUR", "USD"
     ];
 
     /**
