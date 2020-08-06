5bec0dfa44149e71e564e59641138d5a7e35eecc
Grigoruta Adrian <adrian@nordicwebteam.se>
2020-08-06 15:09:24 +0000

    NE-66 Added iframe popup with the Nets payment method on checkout

--- view/frontend/web/js/view/payment/method-renderer/checkout-method.js
+++ view/frontend/web/js/view/payment/method-renderer/checkout-method.js
@@ -7,17 +7,25 @@ define(
         'mage/url',
         'Dibs_EasyCheckout/js/action/before-order'
     ],
-    function ($,Component,url, dibs) {
+    function (
+        $,
+        Component,
+        url,
+        dibs
+    ) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'Dibs_EasyCheckout/payment/checkout'
             },
             continueTodibs: function () {
-                dibs("dibseasycheckout", function () {
-                    $.mage.redirect(url.build('easycheckout') + '?checkRedirect=1');
-                });
+                // dibs("dibseasycheckout", function () {
+                //     $.mage.redirect(url.build('easycheckout') + '?checkRedirect=1');
+                // });
                 return false;
+            },
+            getNetsUrl: function () {
+                return url.build('easycheckout') + '?checkRedirect=1';
             }
         });
     }
