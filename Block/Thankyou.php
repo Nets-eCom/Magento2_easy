9a579547926697fa2c1bb9b4e36fe6ae563d29c2
Niteen Barde <nrbar@nets.eu>
2021-10-21 18:37:58 +0530

    mini cart clear fixes

---
+++ Block/Thankyou.php
@@ -0,0 +1,34 @@
+<?php
+namespace Dibs\EasyCheckout\Block;
+
+class Thankyou extends \Magento\Sales\Block\Order\Totals
+{
+    protected $checkoutSession;
+    protected $customerSession;
+    protected $_orderFactory;
+    
+    public function __construct(
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Sales\Model\OrderFactory $orderFactory,
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $data);
+        $this->checkoutSession = $checkoutSession;
+        $this->customerSession = $customerSession;
+        $this->_orderFactory = $orderFactory;
+    }
+
+    public function getOrder()
+    {
+        return  $this->_order = $this->_orderFactory->create()->loadByIncrementId(
+            $this->checkoutSession->getLastRealOrderId());
+    }
+
+    public function getCustomerId()
+    {
+        return $this->customerSession->getCustomer()->getId();
+    }
+}
