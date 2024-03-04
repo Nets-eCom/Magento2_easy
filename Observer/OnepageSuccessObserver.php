4deea734006623bab7fec163d556cf2c25c1970b
dominik.brozyna <dominik.brozyna@external.nexigroup.com>
2024-03-04 14:12:02 +0000

    Release 04-03-2024
    - Fix: Plugin incompatible with PHP 7.4

--- Observer/OnepageSuccessObserver.php
+++ Observer/OnepageSuccessObserver.php
@@ -2,41 +2,40 @@
 
 namespace Dibs\EasyCheckout\Observer;
 
-use Dibs\EasyCheckout\Helper\Data;
 use Dibs\EasyCheckout\Model\Client\Api\Payment;
 use Magento\Framework\Event\Observer as EventObserver;
 use Magento\Framework\Event\ObserverInterface;
 
 class OnepageSuccessObserver implements ObserverInterface
 {
-
-    protected Data $helper;
-
-    protected Payment $paymentApi;
-
+    private Payment $paymentApi;
 
     public function __construct(
-        \Dibs\EasyCheckout\Helper\Data $helper,
-        \Dibs\EasyCheckout\Model\Client\Api\Payment $paymentApi,
+        Payment $paymentApi
     ) {
-        $this->helper     = $helper;
         $this->paymentApi = $paymentApi;
     }
 
     public function execute(EventObserver $observer)
     {
-        $order   = $observer->getEvent()->getOrder();
-        $payment = $order->getPayment();
-        if ($payment->getMethod() == "dibseasycheckout") {
-            $paymentId = $order->getDibsPaymentId();
-            $storeId   = $order->getStoreId();
-            //Update Card Type in sales_order_payment table in addition_information column.
-            $paymentDetails = $this->paymentApi->getPayment($paymentId, $storeId);
-            $order->getPayment()->setAdditionalInformation(
-                'dibs_payment_method',
-                $paymentDetails->getPaymentDetails()->getPaymentMethod()
-            );
-            $order->save();
+        $order = $observer->getEvent()->getOrder();
+        if ($order === null) {
+            return;
         }
+
+        $payment = $order->getPayment();
+        if ($payment->getMethod() !== "dibseasycheckout") {
+            return;
+        }
+
+        $paymentId = $order->getDibsPaymentId();
+        $storeId = $order->getStoreId();
+        //Update Card Type in sales_order_payment table in addition_information column.
+        $paymentDetails = $this->paymentApi->getPayment($paymentId, $storeId);
+        $order->getPayment()->setAdditionalInformation(
+            'dibs_payment_method',
+            $paymentDetails->getPaymentDetails()->getPaymentMethod()
+        );
+        $order->save();
     }
 }
