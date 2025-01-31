d63cbe6a84a2a225d7be06f52a2446ced4b68c11
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2025-01-31 11:08:39 +0100

    SQNETS-32: add PHPDocs

--- Gateway/Config/Config.php
+++ Gateway/Config/Config.php
@@ -10,21 +10,33 @@ class Config extends \Magento\Payment\Gateway\Config\Config
     public const API_IDENTIFIER = 'api_identifier';
     public const KEY_ENVIRONMENT = 'environment';
 
+    /**
+     * @return bool
+     */
     public function isActive(): bool
     {
         return (bool) $this->getValue(self::KEY_ACTIVE);
     }
 
+    /**
+     * @return mixed|null
+     */
     public function getApiKey()
     {
         return $this->getValue(self::API_KEY);
     }
 
+    /**
+     * @return mixed|null
+     */
     public function getApiIdentifier()
     {
         return $this->getValue(self::API_IDENTIFIER);
     }
 
+    /**
+     * @return string
+     */
     public function getEnvironment(): string
     {
         return $this->getValue(self::KEY_ENVIRONMENT);
