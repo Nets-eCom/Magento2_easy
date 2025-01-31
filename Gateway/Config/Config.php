df7ca8852d43d9189db0ee74fda33f24d40d5d6f
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2025-01-31 11:47:52 +0100

    SQNETS-32: improve gatewayConfig

--- Gateway/Config/Config.php
+++ Gateway/Config/Config.php
@@ -5,42 +5,85 @@ namespace Nexi\Checkout\Gateway\Config;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Payment\Gateway\Config\Config as MagentoConfig;
 use Nexi\Checkout\Model\Config\Source\Environment;
-use NexiCheckout\Factory\Provider\HttpClientConfigurationProvider;
 
 class Config extends MagentoConfig
 {
+    public const CODE = 'nexi';
+    public const KEY_ENVIRONMENT = 'environment';
+    public const KEY_ACTIVE = 'active';
+    public const API_KEY = 'api_key';
+    public const API_IDENTIFIER = 'api_identifier';
+    public const WEBSHOP_TERMS_AND_CONDITIONS = 'webshop_terms_and_conditions_url';
+    public const PAYMENT_TERMS_AND_CONDITIONS = 'payment_terms_and_conditions_url';
+
+    /**
+     * Gateway Config constructor.
+     *
+     * @param ScopeConfigInterface $scopeConfig
+     * @param $methodCode
+     * @param $pathPattern
+     */
     public function __construct(
-        ScopeConfigInterface                             $scopeConfig,
+        ScopeConfigInterface $scopeConfig,
         $methodCode = null,
         $pathPattern = MagentoConfig::DEFAULT_PATH_PATTERN,
     ) {
         parent::__construct($scopeConfig, $methodCode, $pathPattern);
     }
 
-    public const CODE = 'nexi';
-
-    public const KEY_ENVIRONMENT = 'environment';
-    public const KEY_SECRET_KEY  = 'secret_key';
-    public const KEY_ACTIVE      = 'active';
-    public const API_IDENTIFIER  = 'api_identifier';
-
+    /**
+     * @return string
+     */
     public function getEnvironment(): string
     {
         return $this->getValue(self::KEY_ENVIRONMENT);
     }
 
+    /**
+     * @return bool
+     */
     public function isLiveMode(): bool
     {
         return $this->getEnvironment() === Environment::LIVE;
     }
 
+    /**
+     * @return bool
+     */
     public function isActive(): bool
     {
         return (bool)$this->getValue(self::KEY_ACTIVE);
     }
 
-    public function getSecretKey(): string
+    /**
+     * @return string
+     */
+    public function getApiKey(): string
     {
-        return $this->getValue(self::KEY_SECRET_KEY);
+        return $this->getValue(self::API_KEY);
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiIdentifier(): string
+    {
+        return $this->getValue(self::API_IDENTIFIER);
+    }
+
+    /**
+     * @return string
+     */
+    public function getWebshopTermsAndConditions(): string
+    {
+        return $this->getValue(self::WEBSHOP_TERMS_AND_CONDITIONS);
+    }
+
+    /**
+     * @return string
+     */
+    public function getPaymentTermsAndConditions(): string
+    {
+        return $this->getValue(self::PAYMENT_TERMS_AND_CONDITIONS);
     }
 }
