9e972580efee0dd4fbe71f406733dbc8bb87cad3
konrad.konieczny <konrad.konieczny@solteq.com>
2025-02-17 14:54:04 +0100

    SQNETS-49: add conditions and error handling for return action

--- Controller/Hpp/ReturnAction.php
+++ Controller/Hpp/ReturnAction.php
@@ -2,12 +2,14 @@
 
 namespace Nexi\Checkout\Controller\Hpp;
 
+use Exception;
 use Magento\Checkout\Model\Session;
 use Magento\Framework\App\ActionInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Message\ManagerInterface;
 use Magento\Framework\UrlInterface;
 use Magento\Payment\Model\MethodInterface;
 use Magento\Sales\Model\Order;
@@ -15,17 +17,20 @@ use Magento\Sales\Model\Order\Payment\Transaction;
 use Magento\Sales\Model\OrderRepository;
 use Nexi\Checkout\Gateway\Config\Config;
 use Nexi\Checkout\Model\Transaction\Builder;
+use Psr\Log\LoggerInterface;
 
 class ReturnAction implements ActionInterface
 {
     public function __construct(
-        private readonly RedirectFactory  $resultRedirectFactory,
-        private readonly RequestInterface $request,
-        private readonly UrlInterface     $url,
-        private readonly Session          $checkoutSession,
-        private readonly Config           $config,
-        private readonly Builder          $transactionBuilder,
-        private readonly OrderRepository  $orderRepository,
+        private RedirectFactory  $resultRedirectFactory,
+        private RequestInterface $request,
+        private UrlInterface     $url,
+        private Session          $checkoutSession,
+        private Config           $config,
+        private Builder          $transactionBuilder,
+        private OrderRepository  $orderRepository,
+        private LoggerInterface  $logger,
+        private ManagerInterface $messageManager
     ) {
     }
 
@@ -33,40 +38,60 @@ class ReturnAction implements ActionInterface
     {
         $order = $this->checkoutSession->getLastRealOrder();
 
-        if ($order->getPayment()->getAdditionalInformation('payment_id') != $this->request->getParam('paymentid')) {
-            throw new LocalizedException(__('Payment ID does not match.'));
-        }
+        $this->logger->debug(
+            'ReturnAction request: ' . json_encode($this->request->getParams())
+            . ' - Order ID: ' . $order->getIncrementId()
+            . 'http referrer: ' . $this->request->getServer('HTTP_REFERER')
+        );
 
-        if ($this->config->getPaymentAction() == MethodInterface::ACTION_AUTHORIZE) {
-            // create pending invoice and set order status to pending
-            $order->setState(Order::STATE_PENDING_PAYMENT)
-                ->setStatus(Order::STATE_PENDING_PAYMENT);
-            $paymentTransaction = $this->transactionBuilder->build(
-                $order,
-                ['payment_id' => $this->request->getParam('paymentid')],
-                Transaction::TYPE_AUTH
+        try {
+            if ($order->getPayment()->getAdditionalInformation('payment_id') != $this->request->getParam('paymentid')) {
+                throw new LocalizedException(__('Payment ID does not match.'));
+            }
+
+            if ($order->getState() != Order::STATE_NEW) {
+                $this->messageManager->addNoticeMessage(__('Payment already processed'));
+                throw new LocalizedException(__('Payment already processed'));
+            }
+
+            $paymentAction = $this->config->getPaymentAction();
+            $paymentId     = $this->request->getParam('paymentid');
+
+            if ($paymentAction == MethodInterface::ACTION_AUTHORIZE) {
+                $order->setState(Order::STATE_PENDING_PAYMENT)->setStatus(Order::STATE_PENDING_PAYMENT);
+                $paymentTransaction = $this->transactionBuilder->build(
+                    $order,
+                    ['payment_id' => $paymentId],
+                    Transaction::TYPE_AUTH
+                );
+                $order->addRelatedObject($paymentTransaction);
+            } elseif ($paymentAction == MethodInterface::ACTION_AUTHORIZE_CAPTURE) {
+                $order->setState(Order::STATE_PROCESSING)->setStatus(Order::STATE_PROCESSING);
+                $paymentTransaction = $this->transactionBuilder->build(
+                    $order,
+                    ['payment_id' => $paymentId],
+                    Transaction::TYPE_CAPTURE
+                );
+
+                $invoice = $order->prepareInvoice();
+                $invoice->register();
+                $invoice->setTransactionId($paymentTransaction->getTxnId());
+                $invoice->pay();
+
+                $order->addRelatedObject($invoice);
+                $order->addRelatedObject($paymentTransaction);
+            }
+            $this->orderRepository->save($order);
+        } catch (Exception $e) {
+            $this->logger->error($e->getMessage() . ' - ' . $e->getTraceAsString());
+            $this->messageManager->addErrorMessage(
+                __('An error occurred during the payment process. Please try again later.')
             );
 
-            $order->addRelatedObject($paymentTransaction);
-        } elseif ($this->config->getPaymentAction() == MethodInterface::ACTION_AUTHORIZE_CAPTURE) {
-            $order->setState(Order::STATE_PROCESSING)
-                ->setStatus(Order::STATE_PROCESSING);
-            $paymentTransaction = $this->transactionBuilder->build(
-                $order,
-                ['payment_id' => $this->request->getParam('paymentid')],
-                Transaction::TYPE_CAPTURE
+            return $this->resultRedirectFactory->create()->setUrl(
+                $this->url->getUrl('checkout/cart/index', ['_secure' => true])
             );
-
-            $invoice = $order->prepareInvoice();
-            $invoice->register();
-            $invoice->setTransactionId($paymentTransaction->getTxnId());
-            $invoice->pay();
-
-            $order->addRelatedObject($invoice);
-            $order->addRelatedObject($paymentTransaction);
         }
-
-        $this->orderRepository->save($order);
 
         return $this->resultRedirectFactory->create()->setUrl(
             $this->url->getUrl('checkout/onepage/success', ['_secure' => true])
