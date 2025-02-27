e893647625c33fc347a6b84eca6fc5f941e46a89
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2025-02-27 11:20:44 +0100

    SQNETS-34: update PaymentCreated service

--- Model/Webhook/PaymentCreated.php
+++ Model/Webhook/PaymentCreated.php
@@ -34,7 +34,7 @@ class PaymentCreated
     {
         $params = json_decode('{"id":"685dc0ca3c034c8d8ac78e88a577870a","merchantId":100065206,"timestamp":"2025-02-24T13:57:49.2851+00:00","event":"payment.created","data":{"order":{"amount":{"amount":5780,"currency":"EUR"},"reference":"000000020","orderItems":[{"grossTotalAmount":5280,"name":"Orestes Yoga Pant ","netTotalAmount":5280,"quantity":1.0,"reference":"MP10-36-Green","taxRate":0,"taxAmount":0,"unit":"pcs","unitPrice":5280},{"grossTotalAmount":0,"name":"Orestes Yoga Pant -36-Green","netTotalAmount":0,"quantity":1.0,"reference":"MP10-36-Green","taxRate":0,"taxAmount":0,"unit":"pcs","unitPrice":0},{"grossTotalAmount":500,"name":"Flat Rate - Fixed","netTotalAmount":500,"quantity":1.0,"reference":"flatrate_flatrate","taxRate":0,"taxAmount":0,"unit":"pcs","unitPrice":500}]},"paymentId":"f369621ef1b149b5b90b65504506eb75"}}', true);
 
-        $order = $this->webhookDataProvider->loadOrder($params['data']['order']['reference']);
+        $order = $this->webhookDataProvider->loadOrderByPaymentId($params['data']['paymentId']);
         $this->processOrder($order, $params['data']['paymentId']);
 
         $this->orderRepository->save($order);
