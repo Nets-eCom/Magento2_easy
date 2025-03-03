c3680e6b24d1ce4830bcc8a56657ae5ee69c9a61
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2025-03-03 16:18:01 +0100

    SQNETS-34: add refund webhook

--- Model/Webhook/PaymentReservationCreated.php
+++ Model/Webhook/PaymentReservationCreated.php
@@ -1,62 +1,38 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Nexi\Checkout\Model\Webhook;
 
-
 use Magento\Checkout\Exception;
-use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Sales\Api\OrderRepositoryInterface;
-use Magento\Sales\Api\TransactionRepositoryInterface;
 use Magento\Sales\Model\Order;
+use Nexi\Checkout\Model\Webhook\Data\WebhookDataLoader;
 
 class PaymentReservationCreated
 {
     public function __construct(
-        private TransactionRepositoryInterface $transactionRepository,
-        private SearchCriteriaBuilder          $searchCriteriaBuilder,
-        private OrderRepositoryInterface       $orderRepository
+        private OrderRepositoryInterface $orderRepository,
+        private WebhookDataLoader $webhookDataLoader
     ) {
     }
 
     /**
      * ProcessWebhook function for 'payment.reservation.created.v2' event.
+     *
      * @param $response
      * @return void
      * @throws Exception
+     * @throws LocalizedException
      */
     public function processWebhook($response)
     {
         $params = json_decode('{"id":"d60fd4bbaad6454a8c2a4377601c969c","timestamp":"2025-02-24T13:58:29.1396+00:00","merchantNumber":100065206,"event":"payment.reservation.created.v2","data":{"paymentMethod":"Visa","paymentType":"CARD","amount":{"amount":5780,"currency":"EUR"},"paymentId":"f369621ef1b149b5b90b65504506eb75"}}', true);
-        $transaction = $this->loadTransactionByPaymentId($params['data']['paymentId']);
+        $order = $this->webhookDataLoader->loadOrderByPaymentId($params['data']['paymentId']);
 
-        $order = $this->orderRepository->get(reset($transaction)->getOrderId());
         $order->getPayment()->setAdditionalInformation('selected_payment_method', $params['data']['paymentMethod']);
 
         $this->processOrder($order);
         $this->orderRepository->save($order);
-    }
-
-    /**
-     * LoadTransactionByPaymentId function
-     *
-     * @param $paymentId
-     * @return \Magento\Sales\Api\Data\TransactionInterface[]
-     * @throws Exception
-     */
-    private function loadTransactionByPaymentId($paymentId)
-    {
-        try {
-            $searchCriteria = $this->searchCriteriaBuilder
-                ->addFilter('txn_id', $paymentId, 'eq')
-                ->create();
-            $transaction = $this->transactionRepository->getList($searchCriteria)->getItems();
-        } catch (\Exception $e) {
-            throw new Exception(__($e->getMessage()));
-        }
-
-        return $transaction;
     }
 
     /**
