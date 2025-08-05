36f7d7dbf9b5cc828e4d0450d822c8e9be044b82
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2025-08-05 12:45:33 +0200

    SQNETS-145: cancel order on cancel payment

--- Controller/Hpp/CancelAction.php
+++ Controller/Hpp/CancelAction.php
@@ -8,22 +8,28 @@ use Magento\Checkout\Model\Session;
 use Magento\Framework\App\ActionInterface;
 use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Message\ManagerInterface;
 use Magento\Framework\UrlInterface;
+use Magento\Sales\Api\OrderManagementInterface;
 
 class CancelAction implements ActionInterface
 {
     /**
+     * CancelAction constructor.
+     *
      * @param RedirectFactory $resultRedirectFactory
      * @param UrlInterface $url
      * @param Session $checkoutSession
+     * @param OrderManagementInterface $orderManagementInterface
      * @param ManagerInterface $messageManager
      */
     public function __construct(
-        private readonly RedirectFactory $resultRedirectFactory,
-        private readonly UrlInterface $url,
-        private readonly Session $checkoutSession,
-        private readonly ManagerInterface $messageManager
+        private readonly RedirectFactory          $resultRedirectFactory,
+        private readonly UrlInterface             $url,
+        private readonly Session                  $checkoutSession,
+        private readonly OrderManagementInterface $orderManagementInterface,
+        private readonly ManagerInterface         $messageManager
     ) {
     }
 
@@ -36,9 +42,37 @@ class CancelAction implements ActionInterface
     {
         $this->checkoutSession->restoreQuote();
         $this->messageManager->addNoticeMessage(__('The payment has been canceled.'));
+        $this->cancelOrderById($this->checkoutSession->getLastOrderId());
 
         return $this->resultRedirectFactory->create()->setUrl(
             $this->url->getUrl('checkout/cart/index', ['_secure' => true])
         );
+    }
+
+    /**
+     * CancelOrderById function.
+     *
+     * @param $orderId
+     * @return void
+     * @throws LocalizedException
+     */
+    private function cancelOrderById($orderId): void
+    {
+        try {
+            $this->orderManagementInterface->cancel($orderId);
+        } catch (\Exception $e) {
+            $this->logger->critical(sprintf(
+                'Nexi exception during order cancel: %s,\n error trace: %s',
+                $e->getMessage(),
+                $e->getTraceAsString()
+            ));
+
+            // Mask and throw end-user friendly exception
+            throw new LocalizedException(__(
+                'Error while cancelling order.
+                    Please contact customer support with order id: %id to release discount coupons.',
+                ['id' => $orderId]
+            ));
+        }
     }
 }
