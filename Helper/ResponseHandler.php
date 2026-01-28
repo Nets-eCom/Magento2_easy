65ccb1d77ac0593ef19c156fe3e4e30f28b6f6c3
Piotr Karszny <piotr.karszny@external.nexigroup.com>
2026-01-28 10:35:29 +0000

    Merged PR 55312: chore(invoice): add logging
    Related work items: #129005

--- Helper/ResponseHandler.php
+++ Helper/ResponseHandler.php
@@ -1,39 +1,40 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Dibs\EasyCheckout\Helper;
 
+use Dibs\EasyCheckout\Logger\Logger;
 use Dibs\EasyCheckout\Model\Client\DTO\GetPaymentResponse;
+use Magento\Framework\DB\Transaction;
+use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Email\Sender\InvoiceSender;
+use Magento\Sales\Model\Order\Invoice;
+use Magento\Sales\Model\Service\InvoiceService;
 
 class ResponseHandler
 {
-    private \Magento\Sales\Api\OrderRepositoryInterface $orderRepository;
-
-    private \Magento\Sales\Model\Service\InvoiceService $invoiceService;
-
-    private \Magento\Framework\DB\Transaction $transaction;
-
-    private \Magento\Sales\Model\Order\Email\Sender\InvoiceSender $invoiceSender;
-
-    private \Dibs\EasyCheckout\Helper\Data $dibsDataHelper;
-
-    /**
-     * ResponseHandler constructor.
-     *
-     * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
-     */
+    private OrderRepositoryInterface $orderRepository;
+    private InvoiceService $invoiceService;
+    private Transaction $transaction;
+    private InvoiceSender $invoiceSender;
+    private Data $dibsDataHelper;
+    private Logger $logger;
     public function __construct(
-        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
-        \Magento\Sales\Model\Service\InvoiceService $invoiceService,
-        \Magento\Framework\DB\Transaction $transaction,
-        \Magento\Sales\Model\Order\Email\Sender\InvoiceSender $invoiceSender,
-        \Dibs\EasyCheckout\Helper\Data $dibsDataHelper
+        OrderRepositoryInterface $orderRepository,
+        InvoiceService $invoiceService,
+        Transaction $transaction,
+        InvoiceSender $invoiceSender,
+        Data $dibsDataHelper,
+        Logger $logger
     ) {
         $this->orderRepository = $orderRepository;
         $this->invoiceService = $invoiceService;
         $this->transaction = $transaction;
         $this->invoiceSender = $invoiceSender;
         $this->dibsDataHelper = $dibsDataHelper;
+        $this->logger = $logger;
     }
 
     /**
@@ -75,10 +76,9 @@ class ResponseHandler
         if (!$this->dibsDataHelper->getCharge()) {
             return;
         }
-
         try {
             $invoice = $this->invoiceService->prepareInvoice($order);
-            $invoice->setRequestedCaptureCase(\Magento\Sales\Model\Order\Invoice::CAPTURE_ONLINE);
+            $invoice->setRequestedCaptureCase(Invoice::CAPTURE_ONLINE);
             $invoice->register();
             $invoice->save();
             $transactionSave = $this->transaction->addObject(
@@ -93,11 +93,16 @@ class ResponseHandler
             //send notification code
             $order->addCommentToStatusHistory(
                 __('Notified customer about invoice #%1.', $invoice->getId())
-            )->setIsCustomerNotified(true)
-            ->save();
+            )->setIsCustomerNotified(true);
+            $this->orderRepository->save($order);
 
         } catch (\Exception $e) {
-            // We cannot brake transaction
+            $message = sprintf('There was an issue with invoicing the order: %s', $e->getMessage());
+
+            $this->logger->error($message, ['exception' => $e]);
+
+            $order->addCommentToStatusHistory($message)->setIsCustomerNotified(false);
+            $this->orderRepository->save($order);
         }
     }
 }
